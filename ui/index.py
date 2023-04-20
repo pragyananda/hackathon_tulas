1c1564e99a99034aa4276f1ea728fa39b4bdae5c
pragyananda <82259244+pragyananda@users.noreply.github.com>
2023-04-20 15:44:14 +0530

    add ui

---
+++ ui/index.py
@@ -0,0 +1,19 @@
+import streamlit as st 
+
+# Title of the web app
+st.title("TULA'S Hackathon ")
+
+# create a form 
+with st.form(key='my_form'):
+    # create a selectbox
+    option = st.selectbox(
+    'Enter a keyword : ',
+    ('data science', 'web development', 'python'))
+    st.write('You selected:', option)
+    # create a submit button
+    submit_button = st.form_submit_button(label='Submit')
+
+# check if the submit button is clicked
+if submit_button:
+    # if the submit button is clicked, then print the selected option
+    st.write(option)
